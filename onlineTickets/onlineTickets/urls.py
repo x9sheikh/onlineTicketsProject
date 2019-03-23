49ae8a169206513bea692749f5811312644b1d33
x9sheikh <x9sheikh.usama@gmail.com>
2019-03-23 09:08:36 +0500

    Done. Going to reate form

--- onlineTickets/onlineTickets/urls.py
+++ onlineTickets/onlineTickets/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import homePageView,sellTicketsView,createTickertsView
 
 urlpatterns = [
+
+    path('sellTickets/', sellTicketsView),
+    path('', homePageView),
     path('admin/', admin.site.urls),
 ]
--- onlineTickets/pages/views.py
+++ onlineTickets/pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#from onlineTickets.tickets.models import Ticket
+from tickets.models import Ticket
 
 # Create your views here.
+def homePageView(request,*args,**kwargs):
+    context = {}
+    return render(request,"index.html",context=context)
+
+def sellTicketsView(request,*args,**kwargs):
+    tickets =  Ticket.objects.all()
+    context = {
+        'tickets' : tickets,
+    }
+
+    return render(request,"sellTickets.html",context=context)
+

d9df6758229cc17d62b8b6be19a9c9b0f64045b9
x9sheikh <x9sheikh.usama@gmail.com>
2019-03-23 06:56:11 +0500

    models and registered are Done. Now going to view the data in the data base

--- onlineTickets/tickets/models.py
+++ onlineTickets/tickets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    name                = models.CharField(max_length=64)
+    superClassSeat      = models.BooleanField(default=False)
+    student             = models.BooleanField(default=False)
+    seatNo              = models.CharField(max_length=5)
+    movieTitle          = models.CharField(max_length=64)
+    ticketCreateTime    = models.DateTimeField(auto_now_add=True)
